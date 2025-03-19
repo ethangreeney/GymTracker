34968a38e29b45e192192114c753f39616555b4c
katonCODE <danielhuang47@gmail.com>
2025-03-19 16:23:23 +1300

    HELLO

--- GymApp.java
+++ GymApp.java
@@ -1,6 +1,6 @@
 public class GymApp {
     public static void main(String[] args) {
         System.out.println("Gym progress tracking application");
-        System.out.println("Gianni Qu");
+        System.out.println("Gianni Q555555u");
     }
 }
