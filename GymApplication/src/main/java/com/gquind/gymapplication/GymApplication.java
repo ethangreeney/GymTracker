334cc457274c654037b0d15b8462415eace3e871
ethangreeneyy <ethan@greene.nz>
2025-03-19 16:59:16 +1300

    Setting up project space

--- GymApplication/src/main/java/com/gquind/gymapplication/GymApplication.java
+++ GymApplication/src/main/java/com/gquind/gymapplication/GymApplication.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.gquind.gymapplication;
+package GymApplication.src.main.java.com.gquind.gymapplication;
 
 /**
  *
