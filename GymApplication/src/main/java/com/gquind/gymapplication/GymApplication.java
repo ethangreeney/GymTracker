3a04006837804bed94c32729b8c832e5436dac9f
katonCODE <danielhuang47@gmail.com>
2025-03-19 16:50:38 +1300

    Add .gitignore for IDE-specific files

---
+++ GymApplication/src/main/java/com/gquind/gymapplication/GymApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gquind.gymapplication;
+
+/**
+ *
+ * @author daniel
+ */
+public class GymApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
