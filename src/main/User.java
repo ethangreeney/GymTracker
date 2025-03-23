205b56dd6f54bc5c5e421eceb313fe30954f170a
ethangreeneyy <ethan@greene.nz>
2025-03-24 10:01:13 +1300

    added user class and get and set methods

---
+++ src/main/User.java
@@ -0,0 +1,61 @@
+package src.main;
+
+public class User {
+
+    private String username;
+    private String password;
+
+    private String name;
+    private int height;
+    private int age;
+    private int weight;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
