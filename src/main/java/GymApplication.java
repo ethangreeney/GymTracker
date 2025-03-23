673801f216e31b37cfcef11e4414d907ec74e18b
ethangreeneyy <ethan@greene.nz>
2025-03-24 10:33:38 +1300

    adujsted file structure

--- src/main/java/GymApplication.java
+++ src/main/java/GymApplication.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author daniel
- */
-
 import java.util.Scanner;
 
 public class GymApplication {
