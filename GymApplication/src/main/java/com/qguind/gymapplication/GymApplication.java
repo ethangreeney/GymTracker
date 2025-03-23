9f307c183bda5108282be647029f735ea4f09619
ethangreeneyy <ethan@greene.nz>
2025-03-24 08:45:23 +1300

    Created ASCII art and started the login interface

--- GymApplication/src/main/java/com/qguind/gymapplication/GymApplication.java
+++ GymApplication/src/main/java/com/qguind/gymapplication/GymApplication.java
@@ -9,9 +9,30 @@
 
 package com.qguind.gymapplication;
 
+import java.util.Scanner;
+
 public class GymApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scan = new Scanner(System.in);
+
+        String asciiArt = """
+                  _____                _____                _
+                 / ____|              |_   _|              | |
+                | |  __ _   _ _ __ ___  | |  _ __ __ _  ___| | _____ _ __
+                | | |_ | | | | '_ ` _ \\ | | | '__/ _` |/ __| |/ / _ \\ '__|
+                | |__| | |_| | | | | | || |_| | | (_| | (__|   <  __/ |
+                 \\_____|\\___, |_| |_| |_|_____|_|  \\__,_|\\___|_|\\_\\___|_|
+                         __/ |
+                        |___/
+                """;
+
+        System.out.println(asciiArt);
+
+        System.out.println("Welcome to this excersise tracking application! Would you like to:");
+        System.out.println("1: Login");
+        System.out.println("2: Register");
+
     }
 }
