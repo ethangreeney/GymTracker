4f77ce359f62cf3d42cf4d981090a51d0eca1876
ethangreeneyy <ethan@greene.nz>
2025-03-26 12:38:42 +1300

    fixed the hashmap compute if absent

--- src/main/java/PageManager.java
+++ src/main/java/PageManager.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class PageManager {
@@ -5,8 +6,15 @@ public class PageManager {
     public static HashMap<Class<? extends Page>, Page> pageInstanceMap = new HashMap<>();
 
     public void display(Class<? extends Page> pageType) {
+
         var targetPage = pageInstanceMap.computeIfAbsent(pageType, newKey -> {
-            return newKey.getDeclaredConstructor().newInstance();
+            try {
+                return newKey.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                System.out.println("Error occured when creating new instance of page");
+                return null;
+            }
         });
 
     }
