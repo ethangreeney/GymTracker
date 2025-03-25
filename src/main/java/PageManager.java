b851ded20c23ad238876e5dbb84eeec00fcafe84
ethangreeneyy <ethan@greene.nz>
2025-03-26 12:31:08 +1300

    tyring to create the page hashmap

--- src/main/java/PageManager.java
+++ src/main/java/PageManager.java
@@ -5,6 +5,9 @@ public class PageManager {
     public static HashMap<Class<? extends Page>, Page> pageInstanceMap = new HashMap<>();
 
     public void display(Class<? extends Page> pageType) {
+        var targetPage = pageInstanceMap.computeIfAbsent(pageType, newKey -> {
+            return newKey.getDeclaredConstructor().newInstance();
+        });
 
     }
 
