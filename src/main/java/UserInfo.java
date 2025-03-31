55635f0c3e36ffc9a492b336aa04f37263352b05
ethangreeneyy <ethan@greene.nz>
2025-03-31 16:02:43 +1300

    added change user info method

--- src/main/java/UserInfo.java
+++ src/main/java/UserInfo.java
@@ -9,23 +9,46 @@ public class UserInfo implements Page {
         System.out.println("Weight (KG): " + UserManager.currentUser.getWeight());
         System.out.println();
 
-        System.out.println("Enter any input to return");
-        
+        System.out.println("1: Change User Info");
+        System.out.println("2: Main Menu");
 
         handleInput();
-    
 
-
-        
     }
 
     @Override
     public void handleInput() {
 
-        GymApplication.scan.nextLine();
+        int userChoice = GymApplication.stringToInt(GymApplication.scan.nextLine());
 
-        PageManager.navigate(Home.class);
-        
+        switch (userChoice) {
+            case 1:
+                String input;
+                System.out.println("Please enter name: ");
+                input = GymApplication.scan.nextLine();
+                UserManager.currentUser.setName(input);
+                System.out.println("Please enter age: ");
+                input = GymApplication.scan.nextLine();
+                UserManager.currentUser.setAge(GymApplication.stringToInt(input));
+                System.out.println("Please enter height: (cm)");
+                input = GymApplication.scan.nextLine();
+                UserManager.currentUser.setHeight(GymApplication.stringToInt(input));
+                System.out.println("Please enter weight: (kg)");
+                input = GymApplication.scan.nextLine();
+                UserManager.currentUser.setWeight(GymApplication.stringToInt(input));
+
+                PageManager.navigate(UserInfo.class);
+
+                break;
+            case 2:
+                PageManager.navigate(Home.class);
+                break;
+            default:
+                System.out.println("Unknown input, returning to home page");
+                PageManager.navigate(Home.class);
+                break;
+        }
+
     }
 
 }
