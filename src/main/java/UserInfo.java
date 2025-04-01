077459cc6c1729e1dc7620889ee08af1ad1dac08
katonCODE <danielhuang47@gmail.com>
2025-04-02 09:39:15 +1300

    improved change user info functionality🤣

--- src/main/java/UserInfo.java
+++ src/main/java/UserInfo.java
@@ -3,11 +3,11 @@ public class UserInfo implements Page {
     @Override
     public void display() {
 
-        System.out.println("Name: " + UserManager.currentUser.getName());
-        System.out.println("Age: " + UserManager.currentUser.getAge());
+        System.out.println("Name:   " + UserManager.currentUser.getName());
+        System.out.println("Age:    " + UserManager.currentUser.getAge());
         System.out.println("Height: " + UserManager.currentUser.getHeight() + "cm");
         System.out.println("Weight: " + UserManager.currentUser.getWeight() + "kg");
-        System.out.printf("BMI: " + "%.1f" + " (" + bmiClassification() + ")", calculateBMI());
+        System.out.printf("BMI:    " + "%.1f" + " (" + bmiClassification() + ")", calculateBMI());
         System.out.println("\n");
 
         System.out.println("1: Change User Info");
@@ -24,22 +24,7 @@ public class UserInfo implements Page {
 
         switch (userChoice) {
             case 1:
-                String input;
-                System.out.println("Please enter name: ");
-                input = GymApplication.scan.nextLine();
-                UserManager.currentUser.setName(input);
-                System.out.println("Please enter age: ");
-                input = GymApplication.scan.nextLine();
-                UserManager.currentUser.setAge(GymApplication.stringToInt(input));
-                System.out.println("Please enter height: (cm)");
-                input = GymApplication.scan.nextLine();
-                UserManager.currentUser.setHeight(GymApplication.stringToInt(input));
-                System.out.println("Please enter weight: (kg)");
-                input = GymApplication.scan.nextLine();
-                UserManager.currentUser.setWeight(GymApplication.stringToInt(input));
-
-                PageManager.navigate(UserInfo.class);
-
+                changeUserInfo();
                 break;
             case 2:
                 PageManager.navigate(Home.class);
@@ -72,6 +57,54 @@ public class UserInfo implements Page {
         } else {
             return "Obese";
         }
+
+    }
+
+    private void changeUserInfo() {
+
+        String input;
+
+        System.out.println("""
+
+                What would you like to edit:
+
+                1. Name
+                2. Height
+                3. Weight
+                4. Age""");
+        int choice = GymApplication.stringToInt(GymApplication.scan.nextLine());
+
+        switch (choice) {
+            case 1:
+                System.out.println("Please enter name: ");
+                input = GymApplication.scan.nextLine();
+                UserManager.currentUser.setName(input);
+                break;
+
+            case 2:
+            System.out.println("Please enter height: (cm)");
+            input = GymApplication.scan.nextLine();
+            UserManager.currentUser.setHeight(GymApplication.stringToInt(input));
+            break;
+            
+            case 3:
+            System.out.println("Please enter weight: (kg)");
+            input = GymApplication.scan.nextLine();
+            UserManager.currentUser.setWeight(GymApplication.stringToInt(input));
+            break;
+            
+            case 4:
+            System.out.println("Please enter age: ");
+            input = GymApplication.scan.nextLine();
+            UserManager.currentUser.setAge(GymApplication.stringToInt(input));
+            break;
+            
+            default:
+            System.out.println("Incorrect input, please select 1 to 4");
+            changeUserInfo();
+        }
+
+        PageManager.navigate(UserInfo.class);
 
     }
 
