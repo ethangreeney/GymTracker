c0acd7b187758f7feb5e51bb10adfb27925d2eed
katonCODE <danielhuang47@gmail.com>
2025-05-07 12:38:09 +1200

    Started basic framework for GUI conversion

--- src/main/java/GUI/GymApplicationGUI.java
+++ src/main/java/GUI/GymApplicationGUI.java
@@ -1,7 +1,23 @@
 package GUI;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class GymApplicationGUI {
+
+    private static JFrame frame = new JFrame("GymTracking Application");
+
     public static void main(String[] args) {
-        System.out.println("GymApplication.toGUI.ignoreWarnings.please");
+        GymApplicationGUI manager = new GymApplicationGUI();
+        manager.FrameManager();
+
+    }
+
+    private void FrameManager() {
+
+        frame.setSize(500, 500);
+        frame.getContentPane().setBackground(new Color(25, 25, 25));
+        frame.setVisible(true);
     }
 }
