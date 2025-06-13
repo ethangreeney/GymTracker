48b5ae6fedc062c28783af992b0a3fa39e933fe0
ethangreeneyy <ethan@greene.nz>
2025-06-13 17:18:48 +1200

    welcome controller minor change

--- src/main/java/GUI/controller/WelcomeController.java
+++ src/main/java/GUI/controller/WelcomeController.java
@@ -1,22 +1,22 @@
 package GUI.controller;
 
 import GUI.model.DatabaseManager;
+import GUI.view.WelcomePage;
 
 public class WelcomeController {
 
     DatabaseManager db;
     WelcomePage welcomePage;
-    ApplicationControler appController;
-    
+    ApplicationController appController;
 
     public WelcomeController(DatabaseManager model, ApplicationController controller, WelcomePage view) {
         db = model;
         welcomePage = view;
         appController = controller;
-    }
 
-    WelcomePage.addLoginListener(()->appControler.showLoginPage);
-    WelcomePage.addLoginListener(()->appControler.showLoginPage);
-    WelcomePage.addLoginListener(()->appControler.showLoginPage);
+        welcomePage.addLoginListener(() -> appControler.showLoginPage);
+        welcomePage.addLoginListener(() -> appControler.showLoginPage);
+        welcomePage.addLoginListener(() -> appControler.showLoginPage);
+    }
 
 }
