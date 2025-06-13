9e5ff5fe63136312e15207705bfd44f6a4865593
ethangreeneyy <ethan@greene.nz>
2025-06-13 15:59:38 +1200

    fixes names (git capitalisation issue)

--- src/main/java/GUI/view/MainFrame.java
+++ src/main/java/GUI/view/MainFrame.java
@@ -5,17 +5,17 @@ import java.awt.CardLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ainFrame extends JFrame {
+public class MainFrame extends JFrame {
     private CardLayout cardLayout;
     private JPanel cardPanelContainer;
 
-    private omePage home;
-    private oginPage login;
-    private egisterPage register;
+    private HomePage home;
+    private LoginPage login;
+    private RegisterPage register;
     private WorkoutHistoryPage history;
     private WorkoutPage workout;
 
-    public ainFrame() {
+    public MainFrame() {
         setTitle("GymTracker");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 400);
