1c74679f76ab060eb7b29135ada0c95faa84e053
katonCODE <danielhuang47@gmail.com>
2025-06-13 15:46:22 +1200

    Small progress towards GUI

---
+++ src/main/java/GUI/view/mainFrame.java
@@ -0,0 +1,11 @@
+package GUI.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class mainFrame extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel cardPanelContainer;
+
+}
