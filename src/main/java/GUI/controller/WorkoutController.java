e04f2024741046e1bc68b1255d7d07666b794a02
ethangreeneyy <ethan@greene.nz>
2025-06-15 17:51:38 +1200

    created action listeners for the second workout page

--- src/main/java/GUI/controller/WorkoutController.java
+++ src/main/java/GUI/controller/WorkoutController.java
@@ -1,10 +1,12 @@
 package GUI.controller;
 
+import CLI.Exercise;
 import GUI.model.DatabaseManager;
 import GUI.model.Workout;
 import GUI.view.WorkoutPageInterface;
 
 public class WorkoutController {
+
     public WorkoutController(DatabaseManager model, ApplicationController controller, WorkoutPageInterface view) {
 
         Workout currentWorkout = new Workout();
@@ -14,8 +16,28 @@ public class WorkoutController {
 
         view.addFirstNextListener(e -> {
 
+            String workoutName = view.getWorkoutName().trim();
+
+            if (workoutName.equals("")) {
+                view.invalidWorkoutName();
+                return;
+            }
+
             currentWorkout.setName(view.getWorkoutName());
             view.toSecondPage();
+
+        });
+
+        view.addSecondNextListener(e -> {
+
+            String exerciseName = view.getExerciseChoice().trim();
+
+            if (exerciseName.equals("")) {
+                view.invalidExerciseName();
+                return;
+            }
+
+            Exercise currentExercise = new Exercise(exerciseName);
 
         });
 
