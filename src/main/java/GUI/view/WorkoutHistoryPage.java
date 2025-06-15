4178df85a00c26d449d0e49bf3333b715835ae1e
katonCODE <danielhuang47@gmail.com>
2025-06-15 21:30:00 +1200

    Workout history page format

--- src/main/java/GUI/view/WorkoutHistoryPage.java
+++ src/main/java/GUI/view/WorkoutHistoryPage.java
@@ -1,6 +1,10 @@
 package GUI.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -27,6 +31,8 @@ public class WorkoutHistoryPage extends JPanel {
 
     private JPanel topBar;
     private JPanel bottomPanel;
+    private JScrollPane scrollPane;
+    private JScrollPane scrollPane2;
 
     public WorkoutHistoryPage() {
         setBackground(Utilities.backgroundColour);
@@ -53,11 +59,87 @@ public class WorkoutHistoryPage extends JPanel {
 
         add(Box.createRigidArea(Utilities.buttonGap));
 
-        previousWorkouts = new JList<>();
-        previousWorkouts.setAlignmentX(CENTER_ALIGNMENT);
-        previousWorkouts.setMaximumSize(textFieldSize);
-        previousWorkouts.setMinimumSize(textFieldSize);
-        add(previousWorkouts);
+        bottomPanel = new JPanel();
+        bottomPanel.setBackground(this.getBackground());
+
+        bottomPanel.setLayout(new GridLayout(1, 2, 10, 0));
+
+        String[] workoutHistoryData = {
+                "bench",
+                "Interprative steph curry dancing",
+                "Watching Lebron James",
+                "Gaming and Gooning",
+                "Morning Run",
+                "Coaching the youngest person ever",
+                "Fighting Lions",
+                "Learning to be a F student",
+                "Morning Meditation",
+                "Afternoon Yoga",
+                "Hustlers university workout",
+                "Buying a new tesla"
+        };
+
+        String testString = """
+                "bench",
+                "Interprative steph curry dancing",
+                "Watching Lebron James",
+                "Gaming and Gooning",
+                "Morning Run",
+                "Coaching the youngest person ever",
+                "Fighting Lions",
+                "Learning to be a F student",
+                "Morning Meditation",
+                "Afternoon Yoga",
+                "Hustlers university workout",
+                "Buying a new tesla"
+                "bench",
+                "Interprative steph curry dancing",
+                "Watching Lebron James",
+                "Gaming and Gooning",
+                "Morning Run",
+                "Coaching the youngest person ever",
+                "Fighting Lions",
+                "Learning to be a F student",
+                "Morning Meditation",
+                "Afternoon Yoga",
+                "Hustlers university workout",
+                "Buying a new tesla"
+                "bench",
+                "Interprative steph curry dancing",
+                "Watching Lebron James",
+                "Gaming and Gooning",
+                "Morning Run",
+                "Coaching the youngest person ever",
+                "Fighting Lions",
+                "Learning to be a F student",
+                "Morning Meditation",
+                "Afternoon Yoga",
+                "Hustlers university workout",
+                "Buying a new tesla"
+                """;
+
+        previousWorkouts = new JList<>(workoutHistoryData);
+        previousWorkouts.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        previousWorkouts.setAlignmentX(LEFT_ALIGNMENT);
+
+        scrollPane = new JScrollPane(previousWorkouts);
+        scrollPane.setPreferredSize(new Dimension(250, 150));
+        bottomPanel.add(scrollPane, BorderLayout.CENTER);
+
+        displayWorkout = new JTextArea();
+        displayWorkout.setAlignmentX(RIGHT_ALIGNMENT);
+        displayWorkout.setEditable(false);
+        displayWorkout.setLineWrap(true);
+        displayWorkout.setWrapStyleWord(true);
+
+        scrollPane2 = new JScrollPane(displayWorkout);
+        scrollPane.setPreferredSize(new Dimension(250, 150));
+
+        displayWorkout.setText(testString);
+        bottomPanel.add(scrollPane2);
+
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
+        add(bottomPanel);
 
     }
 
