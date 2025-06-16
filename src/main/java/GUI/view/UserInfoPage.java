895fd3a9af7a601fd5c8299c61f810b90d60384e
katonCODE <danielhuang47@gmail.com>
2025-06-16 12:02:41 +1200

    UserInfoPage done

--- src/main/java/GUI/view/UserInfoPage.java
+++ src/main/java/GUI/view/UserInfoPage.java
@@ -1,9 +1,14 @@
 package GUI.view;
 
 import javax.swing.*;
+
+import GUI.model.User;
+
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 
-public class UserInfoPage extends JPanel implements UserInfoPageInterface {
+public class UserInfoPage extends JPanel implements UserInfoPageInferface {
     private String asciiArt = """
 
             ██╗   ██╗███████╗███████╗██████╗     ██╗███╗   ██╗███████╗ ██████╗
@@ -20,8 +25,30 @@ public class UserInfoPage extends JPanel implements UserInfoPageInterface {
     private JPanel topBar;
     private JButton back;
     private JButton edit;
+    private JButton confirm;
+
+    private JLabel nameLabel;
+    private JLabel ageLabel;
+    private JLabel heightLabel;
+    private JLabel weightLabel;
+    private JLabel BMILabel;
 
     private JPanel displayPanel;
+
+    private JLabel nameLabelEdit;
+    private JLabel ageLabelEdit;
+    private JLabel heightLabelEdit;
+    private JLabel weightLabelEdit;
+
+    private JTextField name;
+    private JTextField age;
+    private JTextField height;
+    private JTextField weight;
+
+    private JPanel editPanel;
+
+    private Dimension labelSize = new Dimension(210, 30);
+    private final Dimension textFieldSize = new Dimension(215, 35);
 
     public UserInfoPage() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -46,6 +73,104 @@ public class UserInfoPage extends JPanel implements UserInfoPageInterface {
         userInformation.setAlignmentX(CENTER_ALIGNMENT);
 
         add(userInformation);
+
+        displayPanel = new JPanel();
+        displayPanel.setBackground(this.getBackground());
+        displayPanel.setLayout(new BoxLayout(displayPanel, BoxLayout.Y_AXIS));
+
+        nameLabel = new JLabel("test name");
+        nameLabel.setAlignmentX(CENTER_ALIGNMENT);
+        nameLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        ageLabel = new JLabel("test age");
+        ageLabel.setAlignmentX(CENTER_ALIGNMENT);
+        ageLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        heightLabel = new JLabel("test height");
+        heightLabel.setAlignmentX(CENTER_ALIGNMENT);
+        heightLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        weightLabel = new JLabel("test weight");
+        weightLabel.setAlignmentX(CENTER_ALIGNMENT);
+        weightLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        BMILabel = new JLabel("test BMI");
+        BMILabel.setAlignmentX(CENTER_ALIGNMENT);
+        BMILabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        edit = new JButton("Edit Information");
+        edit.setAlignmentX(CENTER_ALIGNMENT);
+
+        displayPanel.add(nameLabel);
+        displayPanel.add(ageLabel);
+        displayPanel.add(heightLabel);
+        displayPanel.add(weightLabel);
+        displayPanel.add(BMILabel);
+
+        displayPanel.add(Box.createRigidArea(Utilities.buttonGap));
+
+        displayPanel.add(edit);
+
+        add(displayPanel);
+
+        displayPanel.setVisible(false);
+
+        editPanel = new JPanel();
+        editPanel.setBackground(this.getBackground());
+        editPanel.setLayout(new BoxLayout(editPanel, BoxLayout.Y_AXIS));
+
+        nameLabelEdit = new JLabel("Name: ");
+        nameLabelEdit.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(nameLabelEdit);
+
+        name = new JTextField();
+        name.setPreferredSize(textFieldSize);
+        name.setMaximumSize(textFieldSize);
+        name.setMinimumSize(textFieldSize);
+        name.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(name);
+
+        ageLabelEdit = new JLabel("Age: ");
+        ageLabelEdit.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(ageLabelEdit);
+
+        age = new JTextField();
+        age.setPreferredSize(textFieldSize);
+        age.setMaximumSize(textFieldSize);
+        age.setMinimumSize(textFieldSize);
+        age.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(age);
+
+        heightLabelEdit = new JLabel("Height: ");
+        heightLabelEdit.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(heightLabelEdit);
+
+        height = new JTextField();
+        height.setPreferredSize(textFieldSize);
+        height.setMaximumSize(textFieldSize);
+        height.setMinimumSize(textFieldSize);
+        height.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(height);
+
+        weightLabelEdit = new JLabel("Weight: ");
+        weightLabelEdit.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(weightLabelEdit);
+
+        weight = new JTextField();
+        weight.setPreferredSize(textFieldSize);
+        weight.setMaximumSize(textFieldSize);
+        weight.setMinimumSize(textFieldSize);
+        weight.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(weight);
+
+        editPanel.add(Box.createRigidArea(Utilities.buttonGap));
+
+        confirm = new JButton("Confirm Changes");
+        confirm.setAlignmentX(CENTER_ALIGNMENT);
+        editPanel.add(confirm);
+
+        add(editPanel);
+
     }
 
     @Override
@@ -57,6 +182,61 @@ public class UserInfoPage extends JPanel implements UserInfoPageInterface {
     @Override
     public void addEditListener(ActionListener e) {
         edit.addActionListener(e);
+    }
+
+    @Override
+    public void addConfirmListener(ActionListener e) {
+        confirm.addActionListener(e);
+    }
+
+    @Override
+    public String getNewName() {
+        return name.getText();
+    }
+
+    @Override
+    public String getNewAge() {
+        return age.getText();
+    }
+
+    @Override
+    public String getNewHeight() {
+        return height.getText();
+    }
+
+    @Override
+    public String getNewWeight() {
+        return weight.getText();
+    }
+
+    @Override
+    public void toDisplayPanel() {
+        editPanel.setVisible(false);
+        name.setText("");
+        age.setText("");
+        height.setText("");
+        weight.setText("");
+        displayPanel.setVisible(true);
+    }
+
+    @Override
+    public void toEditPanel() {
+        displayPanel.setVisible(false);
+        name.setText("");
+        age.setText("");
+        height.setText("");
+        weight.setText("");
+        editPanel.setVisible(true);
+    }
+
+    @Override
+    public void updateUserInfo(User user) {
+        nameLabel.setText(user.getName());
+        ageLabel.setText(String.valueOf(user.getAge()));
+        heightLabel.setText(String.valueOf(user.getHeight()));
+        weightLabel.setText(String.valueOf(user.getWeight()));
+        int bmiCalc = user.getWeight() / ((user.getHeight() / 100) * (user.getHeight() / 100));
+        BMILabel.setText(String.valueOf(bmiCalc));
     }
 
 }
---
+++ src/main/java/GUI/view/UserInfoPageInferface.java
@@ -0,0 +1,29 @@
+package GUI.view;
+
+import java.awt.event.ActionListener;
+
+import GUI.model.User;
+
+public interface UserInfoPageInferface {
+
+    void addBackListener(ActionListener e);
+
+    void addEditListener(ActionListener e);
+
+    void addConfirmListener(ActionListener e);
+
+    String getNewName();
+
+    String getNewAge();
+
+    String getNewHeight();
+
+    String getNewWeight();
+
+    void toDisplayPanel();
+
+    void toEditPanel();
+
+    void updateUserInfo(User user);
+
+}
